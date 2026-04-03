f47153c95ad3a0760f97ec75c5843e7d3c091b9f
Sahan Erwin <sahan@Sahans-MacBook-Pro.local>
2026-04-03 21:52:09 +0700

    feat: init springboot project

---
+++ src/main/java/com/example/juke/JukeApplication.java
@@ -0,0 +1,13 @@
+package com.example.juke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JukeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JukeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/juke/JukeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JukeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
